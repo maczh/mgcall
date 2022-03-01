647970297a96deb14de1269917ff30b1e2d3eafe
maczh <Micro!070804>
2022-03-01 10:07:40 +0800

    增加Get模式调用

--- mgcall.go
+++ mgcall.go
@@ -79,6 +79,68 @@ func Call(service string, uri string, params map[string]string) (string, error) 
 	}
 }
 
+func Get(service string, uri string, params map[string]string) (string, error) {
+	host, err := getHostFromCache(service)
+	if err != nil || host == "" {
+		discovery := mgconfig.GetConfigString("go.discovery")
+		if discovery == "" {
+			discovery = "nacos"
+		}
+		switch discovery {
+		case "nacos":
+			host = mgconfig.GetNacosServiceURL(service)
+		case "consul":
+			host = mgconfig.GetConsulServiceURL(service)
+		}
+		if host != "" {
+			mgcache.OnGetCache("nacos").Add(service, host, 5*time.Minute)
+		} else {
+			return "", errors.New("微服务获取" + service + "服务主机IP端口失败")
+		}
+	}
+	url := host + uri
+	logs.Debug("Nacos微服务请求:{}\n请求参数:{}", url, params)
+	header := map[string]string{"X-Request-Id": mgtrace.GetRequestId()}
+	resp, err := grequests.Get(url, &grequests.RequestOptions{
+		Params:             params,
+		Headers:            header,
+		InsecureSkipVerify: true,
+	})
+	logs.Debug("Nacos微服务返回结果:{}", resp.String())
+	if err != nil {
+		mgcache.OnGetCache("nacos").Delete(service)
+		discovery := mgconfig.GetConfigString("go.discovery")
+		if discovery == "" {
+			discovery = "nacos"
+		}
+		switch discovery {
+		case "nacos":
+			host = mgconfig.GetNacosServiceURL(service)
+		case "consul":
+			host = mgconfig.GetConsulServiceURL(service)
+		}
+		if host != "" {
+			mgcache.OnGetCache("nacos").Add(service, host, 5*time.Minute)
+		} else {
+			return "", errors.New("微服务获取" + service + "服务主机IP端口失败")
+		}
+		url = host + uri
+		resp, err = grequests.Get(url, &grequests.RequestOptions{
+			Params:             params,
+			Headers:            header,
+			InsecureSkipVerify: true,
+		})
+		logs.Debug("Nacos微服务返回结果:{}", resp.String())
+		if err != nil {
+			return "", err
+		} else {
+			return resp.String(), nil
+		}
+	} else {
+		return resp.String(), err
+	}
+}
+
 //微服务调用其他服务的接口,带header
 func CallWithHeader(service string, uri string, params map[string]string, header map[string]string) (string, error) {
 	host, err := getHostFromCache(service)
